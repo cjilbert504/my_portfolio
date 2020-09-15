af1a4a90a30ac533347e8045a1d038a0eb35e224
Collin Jilbert <cjilbert504@gmail.com>
2020-09-15 16:34:20 -0500

    Implemented show functionality for portfolio module

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,43 +1,47 @@
 class PortfoliosController < ApplicationController
-    def index
-        @portfolio_items = Portfolio.all
-    end
+  def index
+    @portfolio_items = Portfolio.all
+  end
 
-    def new
-        @portfolio_item = Portfolio.new
-    end
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 
-    def edit
-      @portfolio_item = Portfolio.find(params[:id])
-    end
+  def new
+    @portfolio_item = Portfolio.new
+  end
 
-    def create
-        @portfolio_item = Portfolio.new(portfolio_item_params)
-    
-        respond_to do |format|
-          if @portfolio_item.save
-            format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully created.' }
-          else
-            format.html { render :new }
-          end
-        end
-    end
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 
-    def update
-      @portfolio_item = Portfolio.find(params[:id])
+  def create
+    @portfolio_item = Portfolio.new(portfolio_item_params)
 
-      respond_to do |format|
-        if @portfolio_item.update(portfolio_item_params)
-          format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully updated.' }
-        else
-          format.html { render :edit }
-        end
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully created.' }
+      else
+        format.html { render :new }
       end
     end
+  end
 
-    private 
-    
-    def portfolio_item_params
-        params.require(:portfolio_item).permit(:title, :subtitle, :body)
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_item_params)
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
     end
+  end
+
+  private 
+    
+  def portfolio_item_params
+      params.require(:portfolio_item).permit(:title, :subtitle, :body)
+  end
 end
